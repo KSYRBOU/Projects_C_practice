b4230702b011e5f77ddf350bdc0535da9706b149
Konstantin <preator@list.ru>
2022-05-10 22:00:17 +0300

    Third example of homework

--- Lesson_1/Example_002/Program.cs
+++ Lesson_1/Example_002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа принимает на вход три числа
+//и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Enter first digit:");
+double Digit_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second digit:");
+double Digit_2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third digit:");
+double Digit_3 = int.Parse(Console.ReadLine());
+double Max_Digit = Digit_1;
+
+if (Digit_2 > Digit_1) Max_Digit = Digit_2;
+if (Digit_3 > Digit_2) Max_Digit = Digit_3;
+Console.Write("Max digit:");
+Console.WriteLine(Max_Digit);
