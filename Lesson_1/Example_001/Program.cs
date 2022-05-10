18776743624702b31f4f2d6ccc29e304ff97cdfd
Konstantin <preator@list.ru>
2022-05-10 21:30:11 +0300

    Second example of homework

--- Lesson_1/Example_001/Program.cs
+++ Lesson_1/Example_001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter first digit:");
+double Digit_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second digit:");
+double Digit_2 = int.Parse(Console.ReadLine());
+if (Digit_1 > Digit_2)
+{
+    Console.Write("The biggest digit is:");
+    Console.WriteLine(Digit_1);
+    Console.Write("The smalest digit is:");
+    Console.WriteLine(Digit_2);
+}
+else
+{
+    Console.Write("The biggest digit is:");
+    Console.WriteLine(Digit_2);
+    Console.Write("The smalest digit is:");
+    Console.WriteLine(Digit_1);
+}
