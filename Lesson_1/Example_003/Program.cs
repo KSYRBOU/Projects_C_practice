07399972c6d8b0999ee266ea673db082f5f0b55c
Konstantin <preator@list.ru>
2022-05-10 22:37:38 +0300

    Fourth example of homework

--- Lesson_1/Example_003/Program.cs
+++ Lesson_1/Example_003/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа на вход принимает число и 
+//выдаёт, является ли число чётным
+//делится ли оно на два без остатка.
+
+Console.WriteLine("Enter digit:");
+int Digit_1 = int.Parse(Console.ReadLine());
+int Result = Digit_1 % 2;
+if (Result == 0) Console.WriteLine("The digit is Even.");
+else Console.WriteLine("The digit is Odd.");
