26302a53152c07f2f6b3860e4371eb96ad09353b
Konstantin <preator@list.ru>
2022-05-18 14:56:52 +0300

    Check for weekendd

--- Lesson_2/Example_003/Program.cs
+++ Lesson_2/Example_003/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter digit:");
+int num = int.Parse(Console.ReadLine());
+int Saturday = 6;
+int Sunday = 7;
+if (num == Saturday ^ num == Sunday) Console.WriteLine("This is a weekend!");
+else Console.WriteLine("This is not a weekend!");
