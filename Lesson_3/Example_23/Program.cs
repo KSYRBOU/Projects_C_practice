cf200dd9781614f451bfae8f62e18a7eab05a3a6
Konstantin <preator@list.ru>
2022-05-21 09:19:46 +0300

    Raw of cubes for digits

--- Lesson_3/Example_23/Program.cs
+++ Lesson_3/Example_23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Enter digit:");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int degree = 3;
+Console.Write ("Cubes for digits: ");
+while (i <= N)
+{
+    Console.Write(" " + Math.Pow(i, degree));
+    i = i + 1;
+}  
