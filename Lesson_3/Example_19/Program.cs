cb0a17ffe4bfd490fdb0e3d14fe2ab4ef4071737
Konstantin <preator@list.ru>
2022-05-21 11:42:11 +0300

    Check for palindrome with method

--- Lesson_3/Example_19/Program.cs
+++ Lesson_3/Example_19/Program.cs
@@ -1,19 +1,29 @@
 ﻿// Программа принимает на вход пятизначное число
 // и проверяет, является ли оно палиндромом
 
+// Метод определения палиндрома
+string Check_Palindrome(int FirstDigit, int SecondDigit, int ThirdDigit, int ThourthDigit, int FithDigit)
+{;
+    string A = "You digit is a palindrome!";
+    string B = "You digit is not a palindrome!";
+    if (FirstDigit == FithDigit && SecondDigit == ThourthDigit) return A;
+    else return B;
+}
+
 int[] Array = new int[5];
 
 Console.WriteLine("Enter 5 digits:");
 Console.WriteLine("Enter digit 1:");
-Array[0] = int.Parse(Console.ReadLine());
+Array[0] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter digit 2:");
-Array[1] = int.Parse(Console.ReadLine());
+Array[1] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter digit 3:");
-Array[2] = int.Parse(Console.ReadLine());
+Array[2] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter digit 4:");
-Array[3] = int.Parse(Console.ReadLine());
+Array[3] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter digit 5:");
-Array[4] = int.Parse(Console.ReadLine());
+Array[4] = Convert.ToInt32(Console.ReadLine());
 
-if (Array[0] == Array[4] && Array[1] == Array[3]) Console.WriteLine("You digit is a palindrome!");
-else Console.WriteLine("You digit is not a palindrome!"); 
+string Result = Check_Palindrome(Array[0], Array[1], Array[2], Array[3], Array[4]);
+
+Console.WriteLine(Result);
