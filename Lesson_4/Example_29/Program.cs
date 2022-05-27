2aeb414a50fc1bb650390d38af6fed9d1172abf1
Konstantin <preator@list.ru>
2022-05-27 14:44:33 +0300

    Array with numbers

---
+++ Lesson_4/Example_29/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа задаёт массив целых чисел и выводит их на экран.
+//Размер массива определяется пользователем.
+
+Console.WriteLine("Enter size of array:");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int[] Arr = new int[Number];
+int Count = -1;
+Number = Number - 1;
+while (Count < Number)
+{
+    Arr[Number] = new Random().Next(1, 10);
+    Console.Write(" " + Arr[Number]);
+    Number = Number - 1;
+} 
