fdf96fee2b6645e3b2cc77d8bf63aa8e4931b3fc
Konstantin <preator@list.ru>
2022-05-27 13:56:02 +0300

    Sum of digits of the numberr

---
+++ Lesson_4/Example_27/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа принимает на вход число и выдаёт сумму 
+//цифр в числе. Задачу решить без использования строк (string).
+
+Console.WriteLine("Enter number:");
+int Number = int.Parse(Console.ReadLine());
+int Result = 0;
+while (Number > 0)
+{
+    Result = Result + (Number % 10);
+    Number = Number / 10;
+}
+Console.WriteLine($"The sum of digits is {Result}");
