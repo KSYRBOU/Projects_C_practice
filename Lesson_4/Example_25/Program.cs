e412899dc206aa8abb9ec645cec7eae022cc301c
Konstantin <preator@list.ru>
2022-05-27 12:52:35 +0300

    Out of input degree of number

--- Lesson_4/Example_25/Program.cs
+++ Lesson_4/Example_25/Program.cs
@@ -1,3 +1,17 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B. Задачу решить без использования Math.Pow.
 
+Console.WriteLine("Enter digit 1:");
+int D1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter digit 2:");
+int D2 = int.Parse(Console.ReadLine());
+
+int Result = D1;
+int i = 2;
+while (i <= D2)
+{
+    Result = Result * D1;
+    i++;
+}  
+Console.WriteLine($"The {D2} degree of {D1} = {Result}");
