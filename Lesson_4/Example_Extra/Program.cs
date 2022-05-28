30debc9793aa9b8491910c0271e0fd6fc896df1e
Konstantin <preator@list.ru>
2022-05-28 08:32:05 +0300

    Comparison

---
+++ Lesson_4/Example_Extra/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Enter the word!");
+string text = Console.ReadLine();
+int len = text.Length;
+string result = String.Empty;
+
+for (int i = 0; i < len; i++)
+{
+    for (int j = 1; j < len; j++)
+    {
+        char SymbolOne = text[i];
+        char SymbolTwo = text[j];
+        if (SymbolOne == SymbolTwo) result = "True";
+        else result = "False";
+    }
+}
+Console.WriteLine(result);
