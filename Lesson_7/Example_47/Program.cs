3fcab2b7427bfae59ca2cabea8cad28852a9d4d2
Konstantin <preator@list.ru>
2022-06-02 16:32:58 +0300

    Array of doubles

---
+++ Lesson_7/Example_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+double[,] GetArray(int a, int b)
+{
+    double[,] array = new double[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = 50 - rnd.Next(100) + rnd.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Ввведите первый размер массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите второй размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arrResult = GetArray(m, n);
+PrintArray(arrResult);
