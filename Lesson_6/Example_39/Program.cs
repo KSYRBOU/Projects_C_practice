46e8d658a92cfb33baa2a74134a4fa00a69a7f13
Konstantin <preator@list.ru>
2022-06-02 10:27:45 +0300

    Numbers > 0

---
+++ Lesson_6/Example_39/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Enter number of array with space:");
+int [] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.WriteLine(String.Join(" ", array));
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+Console.WriteLine("Total amount of numbers > 0: " + count);
