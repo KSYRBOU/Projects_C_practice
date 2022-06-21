7db8694a5e94d430569aa93a8c53b38a0adf2fa2
Konstantin <preator@list.ru>
2022-06-21 18:44:35 +0300

    3D arrays

---
+++ Lesson_8/Example_60/Program.cs
@@ -0,0 +1,44 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+int[,,] GetArray(int a, int b, int c)
+{
+    int[,,] array = new int[a, b, c];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           for (int l = 0; l < array.GetLength(2); l++)
+           {
+             int rndNumber = rnd.Next(10, 99);
+             if (rndNumber !=  array[i, j, l])
+             {
+                array[i, j, l] = rndNumber;
+             }
+           }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int l = 0; l < array.GetLength(2); l++)
+            {
+                Console.Write(array[i, j, l] + $" Индекс {i},{j},{l} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,,] newArray = GetArray(3, 3, 3);
+
+PrintArray(newArray);
