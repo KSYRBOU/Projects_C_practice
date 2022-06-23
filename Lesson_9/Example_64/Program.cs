4d8e0e23a7565c13a7fb2c305f52afacc4985486
Konstantin <preator@list.ru>
2022-06-23 16:15:19 +0300

    Recurcy numbers down

---
+++ Lesson_9/Example_64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalProgression(int num)
+{
+    num--;
+    Console.Write(num+1);
+    if (num > 0)
+    {
+        NaturalProgression(num);
+    }
+}
+NaturalProgression(number);
