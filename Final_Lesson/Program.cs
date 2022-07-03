a3edc9abccc6300e75ddbec62cca2b1800dd8f01
Konstantin <preator@list.ru>
2022-07-03 14:57:59 +0300

    Метод формирования одномерного массива из строк существующего массива, длинна которых меньше или равна 3 символам

--- Final_Lesson/Program.cs
+++ Final_Lesson/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
-//либо задать на старте выполнения алгоритма. 
+// либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 //Метод наполнения одномерного массива типа string
@@ -20,14 +20,24 @@ string[] GetArray(int a)
 
 void PrintArray(string[] array)
 {
+    Console.Write("Содержание массива: ");
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.WriteLine(array[i]);
+        Console.Write($"{array[i]}, ");
     }
+    Console.WriteLine();
 }
 
-Console.WriteLine("Ввведите количество элементов массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
+//Метод формирования одномерного массива из строк существующего массива, длинна которых меньше или равна 3 символам
 
-string[] arrResult = GetArray(m);
-PrintArray(arrResult);
+string[] ChangeArray(string[] existArray)
+{
+    string[] result = new string[existArray.GetLength(0)];
+    for (int i = 0; i < existArray.GetLength(0); i++)
+    {
+        if (existArray[i].Length <= 3)
+        result[i] = existArray[i];
+    }
+    return result;
+}
+
